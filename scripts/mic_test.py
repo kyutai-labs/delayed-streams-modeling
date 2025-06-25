30a173eee5f2a290cf4187c323444de5a853616e
Laurent <laurent.mazare@gmail.com>
2025-06-25 09:16:03 +0200

    Cleanup the mic example, remove the test.

--- scripts/mic_test.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-import numpy as np
-
-print("Recording 3 seconds...")
-audio = sd.rec(3 * 24000, samplerate=24000, channels=1, dtype='float32')
-sd.wait()
-print("Playing back...")
-sd.play(audio, samplerate=24000)
-sd.wait()
